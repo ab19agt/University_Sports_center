3baa5658a3fcb9f0e631ec16e1fdf7b51cd4444f
Arun <86102167+ab19agt@users.noreply.github.com>
2022-02-08 11:58:56 +0000

    Add files via upload

--- src/the_university_sports_center/TimeTable.java
+++ src/the_university_sports_center/TimeTable.java
@@ -8,43 +8,47 @@ package the_university_sports_centre;
 
 /**
  *
- * @author md19acb
+ * @author ab19agt
  */
 public class TimeTable extends Exercise {
-	
+
 	private String id = "";
 	private String slot = "";
-	private String date ="";
-    private int vacancy = 4;
-     TimeTable(int exercise_id,String name,double price,String id,String date,String slot){
-		super(exercise_id,name,price);
-		this.id=id;
+	private String date = "";
+	private int vacancy = 4;
+
+	TimeTable(int exercise_id, String name, double price, String id, String date, String slot) {
+		super(exercise_id, name, price);
+		this.id = id;
 		this.slot = slot;
-        this.date = date;
-		
+		this.date = date;
+
 	}
+
 	public String getSlotId() {
 		return id;
 	}
+
 	public String getSlot() {
 		return slot;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public int getVacancy() {
 		return vacancy;
 	}
+
 	public void decreaseVacancy() {
 		this.vacancy--;
 	}
+
 	public void increaseVacancy() {
 		this.vacancy++;
 	}
-//private String [] exersiseClass = {"Yoga","Zumba","Aquacise","Box Fit","Body Blitz","sport"};
-
-
-
-
+	// private String [] exersiseClass = {"Yoga","Zumba","Aquacise","Box Fit","Body
+	// Blitz","sport"};
 
 }
